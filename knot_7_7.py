b9a5c1f3d70c264f68caf3c3ce471d052cacd5bc
mingli <mingli.yuan@gmail.com>
2025-05-11 19:35:59 +0800

    add torsion 2 and 3

--- knot_7_7.py
+++ knot_7_7.py
@@ -7,6 +7,7 @@ from sage.all import *
 sys.setrecursionlimit(8912)
 
 
+knot2checker = {}
 knot2relators = {}
 
 
@@ -53,8 +54,7 @@ def calculate(knot_name, R, g, t, r, a_poly, chosen_function, mapping_descriptio
 
     torsion_poly = p_val - q_val
 
-    # result = all(a_result)
-    result = True
+    result = all(a_result)
     if result:
         print('--' * 80)
         print('Knot:', knot_name)
@@ -77,7 +77,10 @@ def check(depth, knot_name):
 
     if knot_name not in knot2relators:
         knot2relators[knot_name] = {}
+    if knot_name not in knot2checker:
+        knot2checker[knot_name] = {}
     relators = knot2relators[knot_name]
+    checker = knot2checker[knot_name]
 
     R = PolynomialRing(QQ, 'a')
     t = R.gen()
@@ -91,21 +94,23 @@ def check(depth, knot_name):
         return
 
     r = M.fundamental_group().relators()[0]
-    if r in relators:
+    if r in checker:
         return
+    checker[r] = True
 
     cond_a = r.count('a') - r.count('A') == 0
     cond_b = r.count('b') - r.count('B') == 0
-    if cond_a:
+    if cond_b:
+        r = r.replace('a', 'c')
+        r = r.replace('A', 'C')
+        r = r.replace('b', 'a')
+        r = r.replace('B', 'A')
+        r = r.replace('c', 'b')
+        r = r.replace('C', 'B')
+    if cond_a or cond_b:
+        relators[r] = True
         chosen_function = claculate_polynomial_by_a
         mapping_description = "'a' as multiplicative, 'b' as additive"
-        relators[r] = True
-    elif cond_b:  # Use elif to ensure only one mapping is chosen
-        chosen_function = claculate_polynomial_by_b
-        mapping_description = "'b' as multiplicative, 'a' as additive"
-        relators[r] = True
-
-    if cond_a or cond_b:
         calculate(knot_name, R, g, t, r, a, chosen_function, mapping_description)
         print('--' * 80)
         print('relator index:', len(relators))
@@ -114,5 +119,5 @@ def check(depth, knot_name):
 
 
 knot2relators['7_7'] = {}
-while len(knot2relators['7_7']) < 100:
+while len(knot2relators['7_7']) < 1000:
     check(0, '7_7')
